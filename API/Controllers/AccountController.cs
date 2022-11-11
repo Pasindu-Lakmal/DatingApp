da95081a2c997a1ecbed31f8094c4d21c538c21f
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-11-11 09:51:00 +0530

    registerandLoginRequest

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -17,16 +17,20 @@ namespace API.Controllers
 
         }
 
+
+        //Register
         [HttpPost("register")]
         public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto){
             
-            if(await UserExists(registerDto.Username))char 
+
+            //when we using action result we can use differrent  http status code like BadRequest
+            if(await UserExists(registerDto.Username)) return BadRequest ("Username is taken");
 
             using var hmac = new HMACSHA512();
 
             var user = new AppUser
             {
-                UserName =registerDto.Username,
+                UserName =registerDto.Username.ToLower(),
                 PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PasswordSalt = hmac.Key
             };
@@ -36,11 +40,37 @@ namespace API.Controllers
             return user;
         }
 
+         // Login
+        [HttpPost("login")]
+        public async Task<ActionResult<AppUser>> Login(LoginDto loginDto){
+           var  user = await _context.Users.SingleOrDefaultAsync(x =>x.UserName == loginDto.Username);
+
+           if(user == null) return Unauthorized("Invalid Username");
+
+           using var hmac = new HMACSHA512(user.PasswordSalt);
+
+           var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+           for(int i = 0; i<computedHash.Length; i++)
+           {
+            if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+           }
+
+            return user;
+        }  
+
+
+
         private async Task<bool> UserExists(string username)
         {
-            return await _context.Users.AnyAsync(x =>x.UserName == username.ToLower())
+            return await _context.Users.AnyAsync(x =>x.UserName == username.ToLower());
 
         }
+
+
+
+       
+
         
     }
 }
