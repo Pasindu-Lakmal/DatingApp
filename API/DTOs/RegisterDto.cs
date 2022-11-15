01a81f03a6b39f65c8bc35524e9224c03b97f996
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-11-15 13:21:00 +0530

    ErrorChekingFieldFuntioningWithButtons

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -8,6 +8,7 @@ namespace API.DTOs
         public String Username { get; set; }
 
         [Required]
+        [StringLength(8, MinimumLength = 4)]
         public String Password { get; set; }
         
     }
