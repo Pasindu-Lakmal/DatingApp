02de4e53855a31ab0fb8ca274f6b0443806e73a5
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-11-17 21:43:49 +0530

    APIupdateFuntionality

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using System.Security.Claims;
 using API.Data;
 using API.DTOs;
 using API.Entities;
@@ -41,5 +42,21 @@ namespace API.Controllers
         {
             return await _userRepository.GetMemberAsync(username);
         }
+
+        [HttpPut]
+        public async Task<ActionResult>UpdateUser(MemberUpdateDto memberUpdateDto)
+        {
+            var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var user = await _userRepository.GetUserbyUsernameAsync(username);
+
+            _mapper.Map(memberUpdateDto, user);
+
+            _userRepository.Update(user);
+
+            if( await _userRepository.SaveAllAsync())return NoContent();
+        
+            return BadRequest("Faild to update user");
+        }
+
     }
 }
