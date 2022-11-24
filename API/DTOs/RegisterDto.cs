94fdb22764fc37288658a9ada043686bbdf5dad0
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-11-24 09:43:41 +0530

    updateto.NET7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -4,12 +4,17 @@ namespace API.DTOs
 {
     public class RegisterDto
     {
-        [Required]
-        public String Username { get; set; }
+        [Required] public String Username { get; set; }
+        [Required] public String Gender { get; set; }
+        
+        [Required] public String KnownAs{ get; set; }
+        [Required] public DateOnly? DateOfBirth { get; set; } //optional to make required work
+        [Required] public String City { get; set; }
+        [Required] public String Country { get; set; }
 
-        [Required]
-        [StringLength(8, MinimumLength = 4)]
+        [Required] [StringLength(8, MinimumLength = 4)]
         public String Password { get; set; }
+        
         
     }
 }
