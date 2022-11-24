7f0fcebcaa19c8f60b3fff27302a62e8183f9501
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-11-24 15:37:48 +0530

    gettingPaginationHeaderCorrectly

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
+import { PaginatedResult, Pagination } from 'src/app/_models/pagination';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -9,12 +10,28 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css'],
 })
 export class MemberListComponent implements OnInit {
-  members$: Observable<Member[]>;
+  // members$: Observable<Member[]>;
+  members: Member[] = [];
+  pagination: Pagination | undefined;
+  pageNumber = 1;
+  pageSize = 5;
+
   constructor(private memberService: MembersService) {}
 
   ngOnInit(): void {
-    //we subscribe our obsrvable using async pipe in tmpleteb
-    this.members$ = this.memberService.getMembers();
+    // this.members$ = this.memberService.getMembers();
+    this.loadMembers();
+  }
+
+  loadMembers() {
+    this.memberService.getMembers(this.pageNumber, this.pageSize).subscribe({
+      next: (response) => {
+        if (response.result && response.pagination) {
+          this.members = response.result;
+          this.pagination = response.pagination;
+        }
+      },
+    });
   }
 
   //it directy get from member service
