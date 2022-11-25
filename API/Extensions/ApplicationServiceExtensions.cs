994f2b3216b76b63c88261eb1fa1aa8af1a57bdf
Pasindu-Lakmal <pasindu1lak@gmail.com>
2022-11-25 16:55:28 +0530

    lastActive&CreatedFiltersAPI&Client

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -11,17 +11,18 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
-           
-            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddScoped<IPhotoService, PhotoService>();
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
             services.AddDbContext<DataContext>(option=>
             {
                 option.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
-
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IPhotoService, PhotoService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            // services.AddAutoMapper(typeof(AutoMapperProfile).Assembly);
+            //change with upper one if any error happen
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddScoped<LogUserActivity>();
             return services;
         }
     }
